4b4b0f6322dcf3a6ea2bc96e521db200c23892a1
Leloup <alexleloup@yahoo.fr>
2022-03-18 19:57:07 +0100

    Add BinaryHeap.java

--- code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,59 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    private final Comparator<T> comparator;
+    private final List<T> heap;
 
-    public T pop() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator = comparator;
+        heap = new ArrayList<>();
+    }
 
-    public T peek() { return null; }
+    /**
+     * Getter for current heap
+     * Unmodifiable list returned
+     * @return unmodifiable representation of current heap
+     */
+    public List<T> getHeap() {
+        return Collections.unmodifiableList(heap);
+    }
 
-    public void push(T element) { }
+    /**
+     *
+     * @return the minimum object and remove it from the BinaryHeap
+     */
+    public T pop() {
+        heap.sort(comparator);
+        T e = heap.get(heap.size()-1);
+        heap.remove(e);
+        return e;
+    }
 
-    public int count() { return 0; }
+    /**
+     *
+     * @return the minimum object but it does not remove it from the BinaryHeap
+     */
+    public T peek() {
+        heap.sort(comparator);
+        return heap.get(heap.size()-1);
+    }
+
+    /**
+     * Add element to heap
+     * @param element element to add
+     */
+    public void push(T element) { heap.add(element);}
+
+    /**
+     * Get size of heap
+     * @return size of heap
+     */
+    public int count() { return heap.size(); }
 
 }
