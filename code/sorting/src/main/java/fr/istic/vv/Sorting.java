964d5f5ec4cabb20fb7a66e965927b1aae0ab97d
Leloup <alexleloup@yahoo.fr>
2022-03-18 19:16:28 +0100

    Update Answer 2

--- code/sorting/src/main/java/fr/istic/vv/Sorting.java
+++ code/sorting/src/main/java/fr/istic/vv/Sorting.java
@@ -3,7 +3,9 @@ import java.util.Comparator;
 
 public class Sorting {
 
+    // ------- Bubble Sort -------
     public static <T>  T[] bubblesort(T[] array, Comparator<T> comparator) {
+        array = array.clone();
         for (int x = 0; x < array.length-1; x++)
             for (int y = 0; y < array.length-x-1; y++)
                 if (comparator.compare(array[y], array[y+1]) > 0) {
@@ -12,11 +14,15 @@ public class Sorting {
         return array;
     }
 
+    // ------- Quick Sort -------
+
     public static <T> T[] quicksort(T[] array, Comparator<T> comparator)  {
+        array = array.clone();
         return quicksort(array, comparator, 0, array.length - 1);
     }
 
     private static <T> T[] quicksort(T[] array, Comparator<T> comparator, int low, int high) {
+        array = array.clone();
         if (low < high) {
             int partition = partition(array, comparator, low, high);
             quicksort(array, comparator, low, partition - 1);
@@ -25,17 +31,14 @@ public class Sorting {
         return array;
     }
 
-    public static <T> T[] mergesort(T[] array, Comparator<T> comparator) { return null; }
-
-
     /**
      * Place all smaller than pivot to left of pivot and all bigger elements to right of pivot
      * @param array input array
      * @param comparator comparator for current Type
      * @param low Index of smaller element and
      * @param high index of bigger element (pivot)
-     * @param <T>
-     * @return
+     * @param <T> type
+     * @return partition position
      */
     private static <T> int partition(T[] array, Comparator<T> comparator, int low, int high) {
         T pivot = array[high];
@@ -51,6 +54,70 @@ public class Sorting {
         swap(array,i + 1 ,high);
         return i + 1;
     }
+
+    // ------- Merge Sort -------
+
+    public static <T> T[] mergesort(T[] array, Comparator<T> comparator) {
+        array = array.clone();
+        if (array.length > 0) {
+            mergesort(array, comparator, 0, array.length - 1);
+        }
+        return array;
+    }
+
+    private static <T> void mergesort(T[] array, Comparator<T> comparator, int low, int high) {
+        if (high <= low){
+            return;
+        }
+        int mid = (low+high)/2;
+        mergesort(array, comparator, low, mid);
+        mergesort(array, comparator, mid+1, high);
+        merge(array, comparator, low, mid, high);
+    }
+
+
+    /**
+     * Merge results into a sorted array
+     * @param array array to merge
+     * @param comparator comparator for current type
+     * @param low array begin
+     * @param mid array mid
+     * @param high array end
+     * @param <T> type
+     */
+    private static <T> void merge(T[] array, Comparator<T> comparator, int low, int mid, int high) {
+        T[] leftArray = (T[]) new Object[mid - low + 1];
+        T[] rightArray = (T[]) new Object[high - mid];
+
+        for (int i = 0; i < leftArray.length; i++)
+            leftArray[i] = array[low + i];
+        for (int i = 0; i < rightArray.length; i++)
+            rightArray[i] = array[mid + i + 1];
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        for (int i = low; i < high + 1; i++) {
+            if (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+                if (comparator.compare(leftArray[leftIndex], rightArray[rightIndex]) < 0) {
+                    array[i] = leftArray[leftIndex];
+                    leftIndex++;
+                } else {
+                    array[i] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+            } else if (leftIndex < leftArray.length) {
+                array[i] = leftArray[leftIndex];
+                leftIndex++;
+            } else if (rightIndex < rightArray.length) {
+                array[i] = rightArray[rightIndex];
+                rightIndex++;
+            }
+        }
+    }
+
+
+    // ------- Utility -------
 
     /**
      * Swap two elements in an array
--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -1,9 +1,35 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
 
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 public class SortingTest {
+
+    Comparator<Integer> comparator = Integer::compareTo;
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean bubblesortArrayIsSorted(@ForAll("randomArrayProvider") Integer[] array) {
+        Integer[] bubbleSortArray = Sorting.bubblesort(array, comparator);
+        return IntStream.range(0, bubbleSortArray.length - 1).noneMatch(i -> comparator.compare(bubbleSortArray[i], bubbleSortArray[i + 1]) > 0);
     }
+
+    @Property
+    boolean mergesortArrayIsSorted(@ForAll("randomArrayProvider") Integer[] array) {
+        Integer[] mergeSortArray = Sorting.mergesort(array, comparator);
+        return IntStream.range(0, mergeSortArray.length - 1).noneMatch(i -> comparator.compare(mergeSortArray[i], mergeSortArray[i + 1]) > 0);
+    }
+
+    @Property
+    boolean quicksortArrayIsSorted(@ForAll("randomArrayProvider") Integer[] array) {
+        Integer[] quickSortArray = Sorting.quicksort(array, comparator);
+        return IntStream.range(0, quickSortArray.length - 1).noneMatch(i -> comparator.compare(quickSortArray[i], quickSortArray[i + 1]) > 0);
+    }
+
+
+    @Provide
+    Arbitrary<Integer[]> randomArrayProvider() {
+        return Arbitraries.integers().array(Integer[].class).ofMinSize(0).ofMaxSize(100);
+    }
+
 }
